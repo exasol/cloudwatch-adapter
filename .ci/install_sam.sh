c1082f6fb2df6456dfe35afb38c0a674b83f566f
Jakob Braun <jakob.braun@posteo.de>
2021-02-02 07:29:20 -0400

    fixes

--- .ci/install_sam.sh
+++ .ci/install_sam.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if sam -v &> /dev/null
+then
+    echo "sam is already installed"
+    exit 0
+fi
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew tap aws/tap
