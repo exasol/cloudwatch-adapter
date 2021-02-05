74e41360e958801ab011c0fdb3be1063ed451999
Jakob Braun <jakob.braun@posteo.de>
2021-02-05 11:34:16 -0400

    #7: Added additional metrics: `NET`, `SWAP`, `DB_RAM_SIZE`, `NODES`

--- src/main/java/com/exasol/cloudwatch/ExasolStatisticsTable.java
+++ src/main/java/com/exasol/cloudwatch/ExasolStatisticsTable.java
@@ -4,5 +4,5 @@ package com.exasol.cloudwatch;
  * Enum of the relevant EXA_STATISTICS tables.
  */
 public enum ExasolStatisticsTable {
-    EXA_DB_SIZE_LAST_DAY, EXA_MONITOR_LAST_DAY, EXA_USAGE_LAST_DAY
+    EXA_DB_SIZE_LAST_DAY, EXA_MONITOR_LAST_DAY, EXA_USAGE_LAST_DAY, EXA_SYSTEM_EVENTS
 }
