aee9208267ffb30ef9e2f48bc5ef0433562f7695
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-12-13 17:46:59 +0100

    Downgrade to localstack 0.12.9
    See https://github.com/localstack/localstack/issues/5115

--- src/test/java/com/exasol/cloudwatch/TestConstants.java
+++ src/test/java/com/exasol/cloudwatch/TestConstants.java
@@ -2,5 +2,5 @@ package com.exasol.cloudwatch;
 
 public class TestConstants {
     public static final String EXASOL_DOCKER_DB_VERSION = "7.1.3";
-    public static final String LOCAL_STACK_IMAGE = "localstack/localstack:0.13.1";
+    public static final String LOCAL_STACK_IMAGE = "localstack/localstack:0.12.9";
 }
