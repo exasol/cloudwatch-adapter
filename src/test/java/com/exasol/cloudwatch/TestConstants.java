7b74596a4cb695a7ee8bd3431201528bc92b25d9
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-04-18 10:03:26 +0200

    Upgrade docker containers

--- src/test/java/com/exasol/cloudwatch/TestConstants.java
+++ src/test/java/com/exasol/cloudwatch/TestConstants.java
@@ -1,6 +1,6 @@
 package com.exasol.cloudwatch;
 
 public class TestConstants {
-    public static final String EXASOL_DOCKER_DB_VERSION = "7.1.18";
-    public static final String LOCAL_STACK_IMAGE = "localstack/localstack:1.4.0";
+    public static final String EXASOL_DOCKER_DB_VERSION = "7.1.19";
+    public static final String LOCAL_STACK_IMAGE = "localstack/localstack:2.0.2";
 }
