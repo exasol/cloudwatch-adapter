f7640a55b0b0342e5c3b0b33441694b35e6f6342
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-01-17 15:48:45 +0100

    Update default exasol version

--- src/test/java/com/exasol/cloudwatch/TestConstants.java
+++ src/test/java/com/exasol/cloudwatch/TestConstants.java
@@ -1,6 +1,6 @@
 package com.exasol.cloudwatch;
 
 public class TestConstants {
-    public static final String EXASOL_DOCKER_DB_VERSION = "7.1.24";
+    public static final String EXASOL_DOCKER_DB_VERSION = "7.1.25";
     public static final String LOCAL_STACK_IMAGE = "localstack/localstack:2.2";
 }
